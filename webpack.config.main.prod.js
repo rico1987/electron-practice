10012d7f0869955e63299fff353c26557d62829e
rico1987 <272355332@qq.com>
2018-02-11 11:23:31 +0800

    init project

---
+++ webpack.config.main.prod.js
@@ -0,0 +1,43 @@
+import webpack from 'webpack';
+import merge from 'webpack-merge';
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import baseConfig from './webpack.config.base';
+import CheckNodeEnv from './internals/scripts/CheckNodeEnv';
+
+CheckNodeEnv('production');
+
+export default merge.smart(baseConfig, {
+  devtool: 'source-map',
+
+  target: 'electron-main',
+
+  entry: './app/main.dev',
+
+  output: {
+    path: __dirname,
+    filename: './app/main.prod.js'
+  },
+
+  plugins: [
+    new UglifyJSPlugin({
+      parallel: true,
+      sourceMap: true
+    }),
+
+    new BundleAnalyzerPlugin({
+      analyzerMode: process.env.OPEN_ANALYZER === 'true' ? 'server' : 'disabled',
+      openAnalyzer: process.env.OPEN_ANALYZER === 'true'
+    }),
+
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+      DEBUG_PROD: 'false'
+    })
+  ],
+
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+});
